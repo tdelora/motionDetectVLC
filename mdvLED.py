d71e2143dee837b0646e9b0ebfc42e093637af32
Thomas De Lora <tdelora@sonic.net>
2025-04-03 16:01:03 -0700

    Start of led status code

---
+++ mdvLED.py
@@ -0,0 +1,11 @@
+"""
+This Raspberry Pi code was developed with assistence from newbiely.com
+newbiely.com Raspberry Pi code is made available for public use without any restriction
+For comprehensive instructions and wiring diagrams, please visit:
+https://newbiely.com/tutorials/raspberry-pi/raspberry-pi-led-rgb
+
+Thank you newbiely.com
+"""
+
+import RPi.GPIO as GPIO
+from time import sleep
