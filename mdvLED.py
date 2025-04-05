9b3e019af7f6a955fa25c4210d88001a777ee4c4
Thomas De Lora <tdelora@sonic.net>
2025-04-05 12:35:55 -0700

    Created dictionaries to handle pin and status color specifications

--- mdvLED.py
+++ mdvLED.py
@@ -12,27 +12,28 @@ from time import sleep
 import mdvUtils
 
 class ledStatusClass:
+
+	gpioPins = {'redPin':13,'greenPin':6,'bluePin':18}
+	statusModes = {'init':"#FF0000",'bored':"#0000FF",'no_motion':"#ff8000",'motion':"#301934",'waiting':"#00FF00"}
+	
 	def __init__(self):
 		self.showLED = False
-		self.redPin = 13
-		# self.greenPin = 12
-		self.greenPin = 6
-		self.bluePin = 18
 
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup([self.redPin, self.greenPin, self.bluePin],GPIO.OUT)
-		self.RED = GPIO.PWM(self.redPin, 1000)
-		self.GREEN = GPIO.PWM(self.greenPin, 1000)
-		self.BLUE = GPIO.PWM(self.bluePin, 1000)
+		GPIO.setup([self.gpioPins["redPin"], self.gpioPins["greenPin"],self.gpioPins["bluePin"]],GPIO.OUT)
+		self.RED = GPIO.PWM(int(self.gpioPins["redPin"]), 1000)
+		self.GREEN = GPIO.PWM(int(self.gpioPins["greenPin"]), 1000)
+		self.BLUE = GPIO.PWM(int(self.gpioPins["bluePin"]), 1000)
 
 	def _duty(self,x, in_min, in_max, out_min, out_max):
     		return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
 	def setColor(self,redVal,greenVal,blueVal):
-		print(f"mdvLED.setColor: r:{redVal} g:{greenVal} b:{blueVal}")
-		self.RED.ChangeDutyCycle(self._duty(redVal, 0, 255, 0, 100))
-		self.GREEN.ChangeDutyCycle(self._duty(greenVal, 0, 255, 0, 100))
-		self.BLUE.ChangeDutyCycle(self._duty(blueVal, 0, 255, 0, 100))
+		if self.showLED:
+			print(f"mdvLED.setColor: r:{redVal} g:{greenVal} b:{blueVal}")
+			self.RED.ChangeDutyCycle(self._duty(redVal, 0, 255, 0, 100))
+			self.GREEN.ChangeDutyCycle(self._duty(greenVal, 0, 255, 0, 100))
+			self.BLUE.ChangeDutyCycle(self._duty(blueVal, 0, 255, 0, 100))
 
 	def start(self,showLED):
 		self.showLED = showLED
@@ -41,7 +42,6 @@ class ledStatusClass:
 			self.RED.start(0)
 			self.GREEN.start(0)
 			self.BLUE.start(0)
-
 
 	def stop(self):
 		if self.showLED:
